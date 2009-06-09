9ccd381653a3ff00413197340c516a3214b624fa
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2009-06-09 23:30:41 +0000

    Keep track of the aki (amazon kernel identifier) that we are using

--- src/ec2-kernel-www/version.mk
+++ src/ec2-kernel-www/version.mk
@@ -1,5 +1,9 @@
 NAME    	= ec2-kernel-www
+# info from: 
+# http://developer.amazonwebservices.com/connect/entry.jspa?externalID=1345
 VERSION 	= 2.6.18
 RELEASE 	= 1
 EC2-NAME  	= $(VERSION)-xenU-ec2-v1.0
 EC2-MODULES	= ec2-modules
+EC2-AKI-I386	= aki-9b00e5f2
+EC2-AKI-X86_64	= aki-9800e5f1
