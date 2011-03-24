ad78332752bba9adfe1231ab4f60eaacff74ea97
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2011-03-24 00:22:49 +0000

    Update boto version.

--- src/boto/version.mk
+++ src/boto/version.mk
@@ -1,7 +1,10 @@
 PKGROOT		= /opt/rocks
 NAME    	= boto
-VERSION 	= 2.0b2
+VERSION 	= 2.0b4
 RELEASE 	= 1
 HASHNAME	= hashlib
 HASHVERSION	= 20081119
 TARBALL_POSTFIX	= tar.gz
+
+## Boto Download Location
+## http://boto.googlecode.com/files/boto-2.0b4.tar.gz
