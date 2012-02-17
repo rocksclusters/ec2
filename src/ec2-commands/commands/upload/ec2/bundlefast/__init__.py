fbc99d2029054f2015d68fc516cb61578fe74621
nnhuy2 <nnhuy2>
2012-02-17 01:53:12 +0000

    *** empty log message ***

--- src/ec2-commands/commands/upload/ec2/bundlefast/__init__.py
+++ src/ec2-commands/commands/upload/ec2/bundlefast/__init__.py
@@ -1,4 +1,4 @@
-# $Id: __init__.py,v 1.4 2012/02/16 20:43:32 nnhuy2 Exp $
+# $Id: __init__.py,v 1.5 2012/02/17 01:53:12 nnhuy2 Exp $
 #
 # Minh Ngoc Nhat Huynh nnhuy2@student.monash.edu
 
@@ -146,7 +146,7 @@ class Command(rocks.commands.HostArgumentProcessor, rocks.commands.upload.comman
 			    ('securitygroups', 'default'),
 			    ('kernelid', 'aki-e5c1218c'),
 			    ('ramdiskid', 'ari-e3c1218a'),
-		    	    ('instancetype', 'm1.large'),
+		    	    ('instancetype', 't1.micro'),
 			    ('outputpath', ),
 			    ('snapshotdesc', ''),
 			    ('aminame', 'Test'),
@@ -176,6 +176,9 @@ class Command(rocks.commands.HostArgumentProcessor, rocks.commands.upload.comman
         	#let's check that the machine is not running
 		print 'physhost is %s; host is %s'  %  (physhost,host)
         	output = self.command('run.host', [ physhost,'/usr/sbin/xm list | grep %s' % host, 'collate=true' ] )
+
+		if len(output) > 1 :
+                    self.abort("The vm " + host + " is still running (" + output + "). Please shut it down before running this command.")
 		
 		#set port to 9000
 		port = 9000
@@ -223,25 +226,11 @@ class Command(rocks.commands.HostArgumentProcessor, rocks.commands.upload.comman
 	            outputpath = outputpath + "/ec2/bundles/" + host
 
 		# -------------------      clear the outputpath and mkdir if it doesn't exist
-	        print "Creating output directories"
-        	output = self.command('run.host', [physhost, 'rm -rf %s' % outputpath, 'collate=true' ] )
-	        output = self.command('run.host', [physhost, 'mkdir -p %s' % outputpath, 'collate=true' ] )
-	        if len(output) > 1:
-        	    self.abort('We can not create the directory ' + outputpath + 'please check that is not mounted or used')
-		
-		# -------------------      clear the outputpath and mkdir if it doesn't exist
                 print "Creating output directories"
                 output = self.command('run.host', [physhost, 'rm -rf %s' % outputpath, 'collate=true' ] )
                 output = self.command('run.host', [physhost, 'mkdir -p %s' % outputpath, 'collate=true' ] )
                 if len(output) > 1:
                     self.abort('We can not create the directory ' + outputpath + 'please check that is not mounted or used')
-
-                if len(output) > 1 :
-                    self.abort("The vm " + host + " is still running (" + output + "). Please shut it down before running this command.")
-
-		
-	        if len(output) > 1 :
-	            self.abort("The vm " + host + " is still running (" + output + "). Please shut it down before running this command.")
 
 		
 		#Record	time from booting up a receiver instance to receiver instance become ready for connecting
@@ -254,25 +243,63 @@ class Command(rocks.commands.HostArgumentProcessor, rocks.commands.upload.comman
     		print 'Launching EC2 instance ...'
     		# launch an instance using this image, key and security groups
     		# by default this will be an m1.large instance
-    		res = image.run(key_name=keypair,security_groups=[securityGroups], kernel_id=kernelId, ramdisk_id=ramdiskId, instance_type=instanceType)
-		
+		#Boot up both dump instance and receiver instance
+    		res = image.run(min_count=2, max_count=2, key_name=keypair,security_groups=[securityGroups], kernel_id=kernelId, ramdisk_id=ramdiskId, instance_type=instanceType)
     		instance = None
+		receiver = None
     		while True:
 			time.sleep(5.0)
 		        res.instances[0].update()
+			res.instances[1].update()
         		print '.',
 		        sys.stdout.flush()
         		dns = res.instances[0].dns_name
+			dns_receiver = res.instances[1].dns_name
 	        	if dns:
 	        	    instance = res.instances[0]
-		            break
+			    receiver = res.instances[1]	
+			    break
 	
 		print 'Instance started. Public DNS: ', instance.dns_name
 		print 'Instance id:', instance.id
 		print 'Instance availability zone: ', instance.placement
-		print 'Waiting for instance to be ready' 
+		#Stop dump instance
+		print 'Stopping dump instance'
+		conn.stop_instances([instance.id], force=True)
 
-		scriptTemp = self.createPingScript(credentialDir + '/' + keypair + '.pem', 'root', instance.dns_name)
+		time.sleep(60)
+
+		#Get volume id /dev/sda1 of dump instance
+		allvols = conn.get_all_volumes()
+		for v in allvols:
+			#If we find volume attached to /dev/sda1 of dump instance we stop
+			if v.attach_data.instance_id == instance.id:
+				break
+
+		print 'Detaching dump instance /dev/sda1'
+		"""print 'Unmount volume'
+		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + instance.dns_name + ' "umount /dev/sda1"'
+		fin, fout = os.popen4(command)
+		print fout.readlines()"""
+
+		print 'Detaching volume'
+		v.detach()
+		while True:
+        		print '.',
+		        sys.stdout.flush()
+			#print newVol.volume_state()
+	        	if v.volume_state() == 'available':
+			    print 'Volume :' + v.id + ' has been successfully detached from instance ' + instance.id
+		            break
+        		time.sleep(1.0)
+			v.update()
+
+		print 'Receiver Instance started. Public DNS: ', receiver.dns_name
+		print 'Receiver Instance id:', receiver.id
+		print 'Receiver availability zone: ', receiver.placement
+		print 'Waiting for receiver instance to be ready' 
+
+		scriptTemp = self.createPingScript(credentialDir + '/' + keypair + '.pem', 'root', receiver.dns_name)
 		retval = os.system('cp %s %s/ping-script.sh' % (scriptTemp, credentialDir))
 	        if retval != 0:
 	            self.abort('Could not copy the script to ping the host: ' + physhost )
@@ -286,22 +313,20 @@ class Command(rocks.commands.HostArgumentProcessor, rocks.commands.upload.comman
 		#Record	time from attaching new EBS volume to EBS become attached
 		startime = datetime.now()
 
-		print 'Creating new volume and attaching to instance ' + instance.id
-		newVol = conn.create_volume(10,instance.placement)
-		if newVol.attach(instance.id, '/dev/sdh'):
-			print 'New volume :' + newVol.id + ' has been created'
+		print 'Attaching new volume to receiver instance'
+		if v.attach(receiver.id, '/dev/sdh'):
+			print 'New volume :' + v.id + ' has been created'
 
-		print 'Attaching new volume to running instance'
 		while True:
         		print '.',
 		        sys.stdout.flush()
 			#print newVol.attachment_state()
-	        	if newVol.attachment_state() == 'attached':
+	        	if v.attachment_state() == 'attached':
 			    time.sleep(5)
-			    print 'New volume :' + newVol.id + ' has been successfully attached to instance ' + instance.id
+			    print 'New volume :' + v.id + ' has been successfully attached to instance ' + receiver.id
 		            break
         		time.sleep(1.0)
-			newVol.update()
+			v.update()
 
 		endtime = datetime.now()
 		print 'Attchment time : ' + str(endtime - startime)
@@ -309,17 +334,17 @@ class Command(rocks.commands.HostArgumentProcessor, rocks.commands.upload.comman
 		#Record	time doing formatting, creating tmp directory, mounting, granting access
 		startime = datetime.now()
 		print 'Formatting volume'
-		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + instance.dns_name + ' "yes | mkfs -t ext3 /dev/sdh"'
+		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + receiver.dns_name + ' "yes | mkfs -t ext3 /dev/sdh"'
 		fin, fout = os.popen4(command)
 		print fout.readlines()
  
 		print 'Creating tmp directory on new volume'
-		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + instance.dns_name + ' "mkdir -p /mnt/tmp"'
+		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + receiver.dns_name + ' "mkdir -p /mnt/tmp"'
 		fin, fout = os.popen4(command)
 		print fout.readlines()
 
 		print 'Mounting new volume'
-		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + instance.dns_name + ' "mount /dev/sdh /mnt/tmp"'
+		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + receiver.dns_name + ' "mount /dev/sdh /mnt/tmp"'
 		fin, fout = os.popen4(command)
 		print fout.readlines()
 		
@@ -331,7 +356,7 @@ class Command(rocks.commands.HostArgumentProcessor, rocks.commands.upload.comman
 		
 		print 'Running server script'
 		#Start udt appserver
-		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + instance.dns_name + ' "bash ~/udt/server.sh"'
+		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + receiver.dns_name + ' "bash ~/udt/server.sh"'
 		fin, fout = os.popen4(command)
 		#print fout.readlines()
 
@@ -370,7 +395,7 @@ class Command(rocks.commands.HostArgumentProcessor, rocks.commands.upload.comman
 		#retval = os.system('scp -qr %s %s:%s/ ' % (credentialDir + '/' + 'libudt.so', physhost, outputpath))
 
 		#Creating upload script
-		scriptTemp = self.createUploadScript('/mnt/rocksimage', instance.dns_name, port)
+		scriptTemp = self.createUploadScript('/mnt/rocksimage', receiver.dns_name, port)
 	        retval = os.system('scp -qr %s %s:%s/upload-script.sh ' % (scriptTemp, physhost, outputpath))
 	        if retval != 0:
 	            self.abort('Could not copy the script to the host: ' + physhost )
@@ -392,30 +417,30 @@ class Command(rocks.commands.HostArgumentProcessor, rocks.commands.upload.comman
 		print 'Uploading time : ' + str(endtime - startime)
 
 		print 'Labeling root'
-		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + instance.dns_name + ' "e2label /dev/sdh /"'
+		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + receiver.dns_name + ' "e2label /dev/sdh /"'
 		fin, fout = os.popen4(command)
 		print fout.readlines()
 
-		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + instance.dns_name + ' "e2label /dev/sdh"'
+		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + receiver.dns_name + ' "e2label /dev/sdh"'
 		fin, fout = os.popen4(command)
 		print fout.readlines()
 		
 		print 'Modifying fstab'
-		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + instance.dns_name + ' "rm -rf /mnt/tmp/etc/fstab"'
+		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + receiver.dns_name + ' "rm -rf /mnt/tmp/etc/fstab"'
 		fin, fout = os.popen4(command)
 		print fout.readlines()
-		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + instance.dns_name + ' "echo \'/dev/sda1 /     ext3    defaults 1 1\' > /mnt/tmp/etc/fstab"'
+		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + receiver.dns_name + ' "echo \'/dev/sda1 /     ext3    defaults 1 1\' > /mnt/tmp/etc/fstab"'
 		fin, fout = os.popen4(command)
-		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + instance.dns_name + ' "echo \'none      /mnt  ext3    defaults 0 0\' >> /mnt/tmp/etc/fstab"'
+		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + receiver.dns_name + ' "echo \'none      /mnt  ext3    defaults 0 0\' >> /mnt/tmp/etc/fstab"'
 		fin, fout = os.popen4(command)
-		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + instance.dns_name + ' "echo \'none      /dev/pts devpts  gid=5,mode=620 0 0\' >> /mnt/tmp/etc/fstab"'
+		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + receiver.dns_name + ' "echo \'none      /dev/pts devpts  gid=5,mode=620 0 0\' >> /mnt/tmp/etc/fstab"'
 		fin, fout = os.popen4(command)
-		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + instance.dns_name + ' "echo \'none      /proc proc    defaults 0 0\' >> /mnt/tmp/etc/fstab"'
+		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + receiver.dns_name + ' "echo \'none      /proc proc    defaults 0 0\' >> /mnt/tmp/etc/fstab"'
 		fin, fout = os.popen4(command)
- 		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + instance.dns_name + ' "echo \'none      /sys  sysfs   defaults 0 0\' >> /mnt/tmp/etc/fstab"'
+ 		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + receiver.dns_name + ' "echo \'none      /sys  sysfs   defaults 0 0\' >> /mnt/tmp/etc/fstab"'
 		fin, fout = os.popen4(command)
 
-		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + instance.dns_name + ' "cat /mnt/tmp/etc/fstab"'
+		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + receiver.dns_name + ' "cat /mnt/tmp/etc/fstab"'
 		fin, fout = os.popen4(command)
 		print fout.readlines()
 	
@@ -424,58 +449,46 @@ class Command(rocks.commands.HostArgumentProcessor, rocks.commands.upload.comman
 
 
 		print 'Unmount volume'
-		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + instance.dns_name + ' "umount /mnt/tmp"'
+		command = 'ssh -t -T -i ' + credentialDir + '/' + keypair + '.pem' + ' root@' + receiver.dns_name + ' "umount /mnt/tmp"'
 		fin, fout = os.popen4(command)
 		print fout.readlines()
 
 		print 'Detaching volume'
-		newVol.detach()
+		v.detach()
 		while True:
         		print '.',
 		        sys.stdout.flush()
 			#print newVol.volume_state()
-	        	if newVol.volume_state() == 'available':
-			    print 'Volume :' + newVol.id + ' has been successfully detached from instance ' + instance.id
+	        	if v.volume_state() == 'available':
+			    print 'Volume :' + v.id + ' has been successfully detached from instance ' + receiver.id
 		            break
         		time.sleep(1.0)
-			newVol.update()
+			v.update()
 
-		#Record	creating snapshot time
-		startime = datetime.now()
+		#Re-attach vol to /dev/sda1 of dump instance
+		if v.attach(instance.id, '/dev/sda1'):
+			print 'New volume :' + v.id + ' has been created'
 
-		print 'Creating snapshot from volume ' + newVol.id
-		snapshot = newVol.create_snapshot(description=snapshotDesc)		
 		while True:
         		print '.',
 		        sys.stdout.flush()
-			#print snapshot.status
-	        	if snapshot.status == 'completed':
-			    print 'Snapshot :' + snapshot.id + ' has been successfully created from volume ' + newVol.id
+			#print newVol.attachment_state()
+	        	if v.attachment_state() == 'attached':
+			    time.sleep(5)
+			    print 'New volume :' + v.id + ' has been successfully attached to instance ' + instance.id
 		            break
         		time.sleep(1.0)
-			snapshot.update()
+			v.update()
 
 		endtime = datetime.now()
-		print 'Creating snapshot time : ' + str(endtime - startime)
-		
-		print 'Register AMI with snapshot ' + snapshot.id
-		ebs = BlockDeviceType()
-		ebs.snapshot_id=snapshot.id
-		block_map = BlockDeviceMapping()
-		block_map['/dev/sda1'] = ebs
-		newAMI = conn.register_image(name=amiName, description=amiDesc, architecture='x86_64', kernel_id = kernelId, ramdisk_id=ramdiskId, root_device_name='/dev/sda1', block_device_map=block_map)
-		print 'New AMI has been successfully created. AMI id : ' + newAMI
 
-		# -----------------------     perform clean up
-		print 'Deleting volume'
-		retVal = conn.delete_volume(newVol.id)
-		if retVal:
-			print 'Volume ' + newVol.id + ' has been successfully deleted'
+		#Turn back on dump instance
+		conn.start_instances([instance.id])
 
 		print 'Terminating receiver instance'
 		#Need to set false to diableApiTermination attr on receiver instance
-		conn.modify_instance_attribute(instance.id, 'disableApiTermination', 'false')
-		conn.terminate_instances([instance.id])
+		conn.modify_instance_attribute(receiver.id, 'disableApiTermination', 'false')
+		conn.terminate_instances([receiver.id])
 
 	def createUploadScript(self, location, dns_name, port):
 	        """this function create the script to upload the VM"""
@@ -485,7 +498,7 @@ sleep 15
 export LD_LIBRARY_PATH=/opt/udt4/lib:$LD_LIBRARY_PATH
 IMAGE_DIR=%s
 cd $IMAGE_DIR
-tar -cSf - . | %s %s %s
+tar -cSf - . --exclude "/proc" --exclude "/dev" --exclude "/media" --exclude "/mnt" --exclude "/sys" --exclude "/tmp" | %s %s %s
 cd /root
 umount $IMAGE_DIR
 """ % (location, '/opt/udt4/bin/appclient', dns_name, port)
