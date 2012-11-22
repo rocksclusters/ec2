14014fff5b7f66716633297f8ec1112ac677f6d9
Luca Clementi <luca.clementi@gmail.com>
2012-11-22 02:53:13 +0000

    First take at rewirting the postinstall script (nothing works at the moment)

--- src/ec2-commands/commands/run/ec2/postinstall/__init__.py
+++ src/ec2-commands/commands/run/ec2/postinstall/__init__.py
@@ -1,201 +1,117 @@
-import re
-import getopt
+# $Id: __init__.py,v 1.4 2012/11/22 02:53:13 clem Exp $
+#
+# @Copyright@
+# 
+# 				Rocks(r)
+# 		         www.rocksclusters.org
+# 		         version 5.5 (Mamba)
+# 		         version 6.0 (Mamba)
+# 
+# Copyright (c) 2000 - 2012 The Regents of the University of California.
+# All rights reserved.	
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+# 
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice unmodified and in its entirety, this list of conditions and the
+# following disclaimer in the documentation and/or other materials provided 
+# with the distribution.
+# 
+# 3. All advertising and press materials, printed or electronic, mentioning
+# features or use of this software must display the following acknowledgement: 
+# 
+# 	"This product includes software developed by the Rocks(r)
+# 	Cluster Group at the San Diego Supercomputer Center at the
+# 	University of California, San Diego and its contributors."
+# 
+# 4. Except as permitted for the purposes of acknowledgment in paragraph 3,
+# neither the name or logo of this software nor the names of its
+# authors may be used to endorse or promote products derived from this
+# software without specific prior written permission.  The name of the
+# software includes the following terms, and any derivatives thereof:
+# "Rocks", "Rocks Clusters", and "Avalanche Installer".  For licensing of 
+# the associated name, interested parties should contact Technology 
+# Transfer & Intellectual Property Services, University of California, 
+# San Diego, 9500 Gilman Drive, Mail Code 0910, La Jolla, CA 92093-0910, 
+# Ph: (858) 534-5815, FAX: (858) 534-7345, E-MAIL:invent@ucsd.edu
+# 
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS''
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# 
+# @Copyright@
+#
+
+
 import sys
 import os
-import tempfile
-from collections import deque
 import subprocess
+import string
 import rocks.commands
 
 class Command(rocks.commands.HostArgumentProcessor, rocks.commands.run.command):
+	"""
+	This script can re-run a kickstart on an already installed host and 
+	perform the remaining part of the script.
+
+	<arg type='string' name='file'>
+	The path to the file containing the kickstart file to be run on this 
+	machine
+	</arg>
+
+
+	<example cmd='run ec2 postinstall /root/ks.cfg"'>
+	Uses the /root/ks.cfg to finalize the installation on this machine.
+	</example>
+	"""
+
+
 	def run(self, params, args):
 		(args, file) = self.fillPositionalArgs(('file',))
 	
 		if not file:
-			self.abort('missing new kcikstartfile')
-		newkickstartfile = file
+			self.abort('missing kcikstartfile')
+		try:
+			kickstartFile = open(file)
+		except IOError:
+			self.abort("Unable to open kickstart file %s." % file)
 
-		print newkickstartfile
-		#return
-		currentkickstartfile = "/root/kickstart/kickstartlite"
-		currentkickstart = open(currentkickstartfile, "r")
-		newkickstart = open(newkickstartfile, "r")
-
-		#Use new dir to stored temporary file
-		newDir = tempfile.mkdtemp(dir='/tmp')
-
-		newpackagesfile = newDir + "/" + "newpackages"
-		currentpackagesfile = newDir + "/" + "currentpackages"
-
-		newpackages = open(newpackagesfile, "w")
-		currentpackages = open(currentpackagesfile, "w")
-
-		flag = False #Use to detect when package section start
-		for line in currentkickstart:
-			if flag:
-				if not line.strip():
-                        		break
-                		print >> currentpackages, line,
-			if re.match("(.*)%packages(.*)", line):
-                		flag = True
-
-		flag = False #Use to detect when package section start
-		for line in newkickstart:
-        		if flag:
-                		if not line.strip():
-                        		break
-                		print >> newpackages, line,
-        		if re.match("(.*)%packages(.*)", line):
-                		flag = True
-
-
-		currentkickstart.close()
-		newkickstart.close()
-		newpackages.close()
-		currentpackages.close()
-
-		packagesdifferentfile = newDir + "/" + "packagesdifferent"
-		packagesdifferent = open(packagesdifferentfile, "w")
-		newpackages = open(newpackagesfile, "r")
-		for line in newpackages:
-			command = 'cat ' + currentpackagesfile  + '| grep "' + line.strip() + '"'
-			proc = subprocess.Popen(command,shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-			if (len(proc.stdout.readlines()) ==  0):
-				print >> packagesdifferent, line,
-		packagesdifferent.close()
-		packagesdifferent = open(packagesdifferentfile, "r")
-		#Install missing packages
-		packagesToInstall = ''
-		for line in packagesdifferent:
-			packagesToInstall = packagesToInstall + line + ' '
-
-		#Install new packages
-		#Test
-		command = "yum install " + packagesToInstall
-		proc = subprocess.call(command,shell=True)
-		newnodesfile = newDir + "/" + "newnodes"
-		currentnodesfile = newDir + "/" + "currentnodes"
 		
-		newnodes = open(newnodesfile, "w")
-		currentnodes = open(currentnodesfile, "w")
-		currentkickstart = open(currentkickstartfile, "r")
+		for line in kickstartFile:
+			if line.startswith("%packages"):
+				break
+		#now we start with the packages
+		packages = []
+		for line in kickstartFile:
+			if len(line.strip()) == 0:
+				pass
+			elif line.startswith("%"):
+				break
+			else:
+				packages.append(line.strip())
+		print "newPakcages: ", string.join(packages, ' ')
+		
 
-		flag = False #Use to detect when package section start
-		for line in currentkickstart:
-        		if flag:
-                		if not line.strip():
-                        		break
-                		print >> currentnodes, line,
-        		if re.match("(.*)\./nodes(.*)", line):
-                		flag = True
-		currentnodes.close()
+		#
+		# get current installed rpms list
+		#
+                #stdout = subprocess.Popen( ['rpm','-qa'], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                #        stderr=subprocess.PIPE).stdout
+                #installedRpms = stdout.read().strip()
+		#installedRpms = installedRpms.split('\n')
 
-		newkickstart = open(newkickstartfile ,"r")
-		flag = False #Use to detect when package section start
-		for line in newkickstart:
-        		if flag:
-                		if not line.strip():
-                        		break
-                		print >> newnodes, line,
-        		if re.match("(.*)\./nodes(.*)", line):
-                		flag = True
 
-		newnodes.close()
 
-		nodesdifferentfile = newDir + "/" + "nodesdifferent"
-		nodesdifferent = open(nodesdifferentfile, "w")
-		newnodes = open(newnodesfile, "r")
-		for line in newnodes:
-        		command = 'cat ' + currentnodesfile  + '| grep "' + line.strip()  + '"'
-			proc = subprocess.Popen(command,shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-        		if (len(proc.stdout.readlines()) ==  0):
-				tmpline = line.split(" ")[1]
-				#print tmpline
-				if not (tmpline == './nodes/partitions-save.xml' or tmpline == './nodes/grub-client.xml' or tmpline == './nodes/resolv.xml' or tmpline == './nodes/routes-client.xml' or tmpline == './nodes/kernel.xml' or tmpline == './nodes/client-firewall.xml' or tmpline == './nodes/ntp-client.xml'):
-                			print >> nodesdifferent, line.split(" ")[1] +"\n",
-
-		#Get post section
-		flag = False #Use to detect when post section begin
-		currentFile = None
-		queuePostSectionFile = deque()
-		for line in newkickstart:
-        		if re.match("(.*)%post(.*)", line):
-                		if not currentFile is None:
-                        		currentFile.close()
-                		tup = tempfile.mkstemp(prefix='tmp', dir=newDir)
-                		currentFile = os.fdopen(tup[0], 'w')
-                		flag = True
-                		queuePostSectionFile.append(tup[1])
-        		if flag:
-                		if not re.match("(.*)%post(.*)", line):
-                        		print >> currentFile, line,
-		print len(queuePostSectionFile)
-		#Filter the post section file.
-		#Remove all begin post section, end post section log.
-		currentPostSectionFile = queuePostSectionFile.popleft()
-		lastNodeXML = '' #Used to determine if we finish all the post section for a given node
-		counter = 0
-		postsectionfilelist = deque()
-		while (len(queuePostSectionFile) > 0 ):
-        		command = 'cat ' + currentPostSectionFile + '| grep ": begin post section"'
-       			proc = subprocess.Popen(command,shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-        		output = proc.stdout.readlines()
-        		if (len(output) > 0):
-                		currentNodeXML = output[0].strip().split(":")[0]
-                		newFlag = False
-				nodesdifferent = open(nodesdifferentfile, "r")
-				for ln in nodesdifferent:
-					if ln.strip() == currentNodeXML:
-						newFlag = True
-						break
-
-				#print currentNodeXML
-				#command = 'cat ' + nodesdifferentfile + '| grep "' + currentNodeXML + '"'
-                		#print command
-				#proc = subprocess.Popen(command,shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-				#result = proc.stdout.readlines()
-				#print result
-				if newFlag:
-                        		#Remove this begin post section
-                        		command = 'rm -rf ' + currentPostSectionFile
-                        		proc = subprocess.Popen(command,shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-                        		#Get all post section between start post section to end post section
-                        		if (currentNodeXML != lastNodeXML):
-                                		counter = 0
-                                		lastNodeXML = currentNodeXML
-                        		isLoop = True
-                        		while isLoop:
-                                		currentPostSectionFile = queuePostSectionFile.popleft()
-                                		command = 'cat ' + currentPostSectionFile + '| grep "' + currentNodeXML+ ': end post section"'
-						#print command
-						proc = subprocess.Popen(command,shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-						if (len(proc.stdout.readlines()) > 0):
-                                        		#Remove this end post section
-                                        		command = 'rm -rf ' + currentPostSectionFile
-                                        		proc = subprocess.Popen(command,shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-                                       			isLoop = False
-                                		else: #This is the section that we are interested in
-                                        		#queueNode.append(currentPostSectionFile)
-                                        		counter = counter + 1
-                                        		newName = newDir + '/' + currentNodeXML.split("/")[2].split(".")[0] + str(counter)
-							command = 'mv ' + currentPostSectionFile + ' ' + newName
-                                        		proc = subprocess.Popen(command,shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-                					postsectionfilelist.append(newName)
-				else:
-                        		command = 'rm -rf ' + currentPostSectionFile
-                        		proc = subprocess.Popen(command,shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-                        		currentPostSectionFile = queuePostSectionFile.popleft()
-        		else:
-				command = 'rm -rf ' + currentPostSectionFile
-                		proc = subprocess.Popen(command,shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-                		currentPostSectionFile = queuePostSectionFile.popleft()
-
-		#Execute missing post section
-		while (len(postsectionfilelist) > 0):
-			line = postsectionfilelist.popleft()
-			command = "bash " + line
-        		proc = subprocess.Popen(command,shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-
-RollName = "ec2"
-
-RollName = "ec2"
